df56987fec53b9e6ac248e7004f98f9b440cb0b1
literandltx <romanskovronn@gmail.com>
2026-01-18 22:49:27 +0200

    init commit

---
+++ src/test/java/com/example/timer_backend/TimerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
