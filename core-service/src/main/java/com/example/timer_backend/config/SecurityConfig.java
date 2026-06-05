9ab0a900f9075ce88a6445e26efa2180c6f21817
literandltx <romanskovronn@gmail.com>
2026-06-05 17:24:08 +0300

    feat: add actuator

--- core-service/src/main/java/com/example/timer_backend/config/SecurityConfig.java
+++ core-service/src/main/java/com/example/timer_backend/config/SecurityConfig.java
@@ -28,6 +28,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
     private static final String[] AUTH_WHITE_LIST = {
             "/api/v1/auth/**",
+            "/actuator/health",
             "/v3/api-docs/**",
             "/v2/api-docs/**",
             "/swagger-ui/**",
